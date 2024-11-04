3c6ea63ff89234bd754ea559ebc7f4065cf89178
blentz <brendan.lentz@opsysinc.com>
2024-11-04 13:53:06 -0700

    CA-166 Fix Python errors
    This commit fixes a warning about invalid escaped characters in the create-db.py script.

--- sql/scripts/create-db.py
+++ sql/scripts/create-db.py
@@ -6,9 +6,9 @@ import os
 import sqlite3
 import regex as re
 
-prefixed_bcvwp = re.compile("^[onON]\d")
-gloss_needing_cleanup = re.compile('^(.+\..+)+$')
-punctuation_or_whitespace_only = re.compile('^[\p{P}\s]*$', re.U)
+prefixed_bcvwp = re.compile(r"^[onON]\d")
+gloss_needing_cleanup = re.compile(r'^(.+\..+)+$')
+punctuation_or_whitespace_only = re.compile(r'^[\p{P}\s]*$', re.U)
 
 
 def sanitize_bcvwp(bcv_id):
